2fbffee9516d2394d3761aecf1b811a25bb86afb
Yang Cao <cyrilcaoyang@gmail.com>
2023-07-28 00:13:32 +0000

    Update significant_digits.py

--- src/etoad/DataAnalyzer/AnalysisUtils/significant_digits.py
+++ src/etoad/DataAnalyzer/AnalysisUtils/significant_digits.py
@@ -12,5 +12,5 @@ def significant_digits(number: Union[int, float, np.float64], no_digits: int = 3
         no_digits: Number of significant figures.
     """
     no_decimal_places = -int(floor(log10(abs(number)))) + no_digits - 1
-    round_number = float(round(number, no_decimal_places))
-    return round_number
+    return float(round(number, no_decimal_places))
+    
