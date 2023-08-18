35addc76109a58726be99155dcb6035afc3fd44e
cyrilcoayang <cyrilcaoyang@gmail.com>
2023-08-18 14:09:44 -0400

    improved the test script

--- Tests/demo_CV_const_scan_rate.py
+++ Tests/demo_CV_const_scan_rate.py
@@ -4,10 +4,11 @@ from pathlib import Path
 
 from etoad.HardwareController.Potentiostat.EChemController import EChemController
 from etoad.Interface import GraphicalInterface
-from etoad.Utils import timestamp_datetime
+from etoad.Utils import timestamp_datetime, ThreadWithReturn
 
 """
-    This python script demonstrate the CV scans with constant scan rates.
+    This python script demonstrate the CV scans with constant scan rates, without using the Workflow Manager
+    Without the usage of the GUI, the experiment can be run in the background.
 """
 
 # ========== Sample Settings Below ========== #
@@ -25,26 +26,43 @@ Simulation: bool = False    # This option can turn ON/OFF the Simulation Mode
 
 # ========== Sample Settings Above ========== #
 
-PARENT_DIR = Path(__file__).parent
-with open(PARENT_DIR / "test_settings" / "file_settings") as file:
-    DATA_DIR = Path(file.read())
 
-logger = GraphicalInterface(
-    logging_config=PARENT_DIR / "test_settings" / "logger_settings.json",
-    log_file=DATA_DIR / "Logs" / f"{timestamp_datetime()}_{Sample_Name}_CV_const_scan_rate.log",
-    disable_gui=Disable_GUI
-)
+def define_paths() -> tuple:
+    """
+    This function defines the path of the working and data directory.
+    """
+    parent_dir = Path(__file__).parent
+    with open(parent_dir / "test_settings" / "file_settings") as file:
+        data_dir = Path(file.read())
+    return parent_dir, data_dir
 
 
-def do_measurement():
+def make_logger(sample_name: str, disable_gui: bool) -> GraphicalInterface:
+    """
+    This function creates a logger for the experiment.
+    """
+    parent_dir, data_dir = define_paths()
+    logger = GraphicalInterface(
+        logging_config=parent_dir / "test_settings" / "logger_settings.json",
+        log_file=data_dir / "Logs" / f"{timestamp_datetime()}_{sample_name}_CV_const_scan_rate.log",
+        disable_gui=disable_gui
+    )
+    logger.sample_name = sample_name
+    return logger
+
+
+def do_measurement(simulation_mode: bool, logger: GraphicalInterface) -> None:
+    """
+    This function performs the CV scans with constant scan rates.
+    """
+    parent_dir, data_dir = define_paths()
     potentiostat = EChemController(
-        config_file=PARENT_DIR / "test_settings" / "potentiostat_settings.json",
+        config_file=parent_dir / "test_settings" / "potentiostat_settings.json",
         logger=logger,
-        simulation_mode=Simulation,
+        simulation_mode=simulation_mode,
     )
 
-    logger.sample_name = Sample_Name
-    logger.info(f"*** Starting Experiment: CV Scans of {logger.sample_name}.")
+    logger.info(f"Starting Experiment: CV Scans of {logger.sample_name}.")
 
     results = potentiostat.do_measurement(
         technique="CV",
@@ -67,16 +85,19 @@ def do_measurement():
     )
 
     # saving the data before disconnection
-    filename = DATA_DIR / "Data" / f"CV_Const_ScanRate_{logger.sample_name}_{timestamp_datetime()}.csv"
+    filename = data_dir / "Data" / f"CV_Const_ScanRate_{logger.sample_name}_{timestamp_datetime()}.csv"
     numpy.savetxt(filename, results, delimiter=',')
-    logger.info(f"<<< Result of CV scans of {logger.sample_name} is saved as {filename}.")
+    logger.info(f"Result of CV scans of {logger.sample_name} is saved as {filename}.")
     potentiostat.disconnect()
     logger.stop_gui()
 
 
-worker_thread = threading.Thread(target=do_measurement)
-worker_thread.start()
-logger.start_gui()
-worker_thread.join()
+if __name__ == "__main__":
+
+    gui_logger = make_logger(Sample_Name, Disable_GUI)
+    worker_thread = ThreadWithReturn(target=do_measurement, args=(Sample_Name, Simulation, gui_logger))
+    worker_thread.start()
+    gui_logger.start_gui()
+    worker_thread.join()
 
 
--- Tests/demo_sampling_system.py
+++ Tests/demo_sampling_system.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
-from src.etoad.HardwareController import SamplingSystem
-from src.etoad.Utils import timestamp_datetime
-from src.etoad.Interface import GraphicalInterface
+from etoad.HardwareController import SamplingSystem
+from etoad.Utils import timestamp_datetime
+from etoad.Interface import GraphicalInterface
+from etoad.Utils import ThreadWithReturn
 
 """
     This python script demonstrate the Sampling System.
@@ -14,43 +15,74 @@ Sample_Name = "K4[Fe(CN)6]"     # Name of the Chemical Solution
 Source_Port = 9                 # The Port from which the Sample will be added.
 Sample_Vol = 0.5                # The volume of sample in mL (< 5mL) to be diluted to 5 mL in the Cell.
 
-Clean_Up: bool = False          # Cleaning Up the EChem Reactor by washing the cell with 3 x 5 mL Solvent.
-Disable_GUI: bool = True        # We are disabling GUI for simple liquid transfer.
-
+Disable_GUI: bool = False        # GUI can be disabled for simple liquid transfer.
+Clean_Up: bool = True
 # ========== Test Settings Above ========== #
 
-PARENT_DIR = Path(__file__).parent
-with open(PARENT_DIR / "test_settings" / "file_settings") as file:
-    DATA_DIR = Path(file.read())
 
-# We are not using GUI for simple liquid transfer
-logger = GraphicalInterface(
-    logging_config=PARENT_DIR / "test_settings" / "logger_settings.json",
-    log_file=DATA_DIR / "Logs" / f"{timestamp_datetime()}_{Sample_Name}_transfer_dilution.log",
-    disable_gui=Disable_GUI
-)
+def define_paths() -> tuple:
+    """
+    This function defines the path of the working and data directory.
+    """
+    parent_dir = Path(__file__).parent
+    with open(parent_dir / "test_settings" / "file_settings") as file:
+        data_dir = Path(file.read())
+    return parent_dir, data_dir
 
-# The EChem Reactor is filled with electrolyte solution by default.
-sampler = SamplingSystem(
-    config_file=PARENT_DIR / "test_settings" / "sampler_settings.json",
-    logger=logger,
-    pump_wash=0,
-    cell_filled=True
-)
 
-# Transfers 0.5 mL from the sample position 9 to the cell
-sampler.transfer_to_cell(
-    source_port=Source_Port,
-    volume=Sample_Vol,
-    wash_line=True
-)
+def make_logger(sample_name: str, disable_gui: bool) -> GraphicalInterface:
+    """
+    This function creates a logger for the experiment.
+    """
+    parent_dir, data_dir = define_paths()
+    logger = GraphicalInterface(
+        logging_config=parent_dir / "test_settings" / "logger_settings.json",
+        log_file=data_dir / "Logs" / f"{timestamp_datetime()}_{sample_name}_transer_dilution.log",
+        disable_gui=disable_gui
+    )
+    logger.sample_name = sample_name
+    return logger
 
-sampler.dilute_cell(volume=5)
 
-if Clean_Up:
-    sampler.wash_cell(
-         wash_volume=5.0,
-         cycles=3
+def sampler_test(
+        source_port: int,
+        sample_vol: float,
+        clean_up: bool,
+        logger: GraphicalInterface
+) -> None:
+    """
+    This function performs the Sampling Demonstration.
+    """
+    parent_dir, data_dir = define_paths()
+    sampler = SamplingSystem(
+        config_file=parent_dir / "test_settings" / "sampler_settings.json",
+        logger=logger,
+        pump_wash=0,
+        cell_filled=True
     )
 
-sampler.disconnect()
+    # Transfers 0.5 mL from the sample position 9 to the cell
+    sampler.transfer_to_cell(
+        source_port=source_port,
+        volume=sample_vol,
+        wash_line=True
+    )
+    sampler.dilute_cell(volume=5)
+
+    if clean_up:
+        sampler.wash_cell(
+             wash_volume=5.0,
+             cycles=3
+        )
+    sampler.disconnect()
+    logger.stop_gui()
+
+
+if __name__ == "__main__":
+    gui_logger = make_logger(sample_name=Sample_Name, disable_gui=Disable_GUI)
+    worker_thread = ThreadWithReturn(target=sampler_test, args=(Source_Port,Sample_Vol,Clean_Up,gui_logger))
+    worker_thread.start()
+    gui_logger.start_gui()
+    worker_thread.join()
+
+
