61a5e27fb2dce45d58ac7a1063a01e05ca4e4dee
Yang Cao <cyrilcaoyang@gmail.com>
2023-08-20 20:57:50 +0000

    Update file __init__.py

--- Tests/test_utils/__init__.py
+++ Tests/test_utils/__init__.py
@@ -1,2 +1,2 @@
 from .PathFinder import data_path
-from .MakeObjects import mk_logger, mk_sampler, mk_potentiostat, mk_analyzer
+from .MakeObjects import mk_logger, mk_sampler, mk_potentiostat, mk_analyzer, mk_workflow_manager, mk_csv
